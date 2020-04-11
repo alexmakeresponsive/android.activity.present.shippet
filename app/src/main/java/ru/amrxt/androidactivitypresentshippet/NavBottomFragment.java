0dd8e09150ac1165651dc25649fb98948a92e4bc
amr <alexmakeresponsive@gmail.com>
2020-04-11 08:45:08 +0600

    add eventListener for button in fragment

--- app/src/main/java/ru/amrxt/androidactivitypresentshippet/NavBottomFragment.java
+++ app/src/main/java/ru/amrxt/androidactivitypresentshippet/NavBottomFragment.java
@@ -1,15 +1,20 @@
 package ru.amrxt.androidactivitypresentshippet;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 
 public class NavBottomFragment extends Fragment {
@@ -17,6 +22,23 @@ public class NavBottomFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nav_bottom, container, false);
+        View view = inflater.inflate(R.layout.fragment_nav_bottom, container, false);
+
+        Button button = (Button) view.findViewById(R.id.button1);
+               button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+
+                Button button = (Button) view.findViewById(R.id.button1);
+
+
+
+                System.out.println(button.getText());
+            }
+        });
+
+        return view;
     }
 }
---
+++ app/src/main/java/ru/amrxt/androidactivitypresentshippet/Secondary1Activity.java
@@ -0,0 +1,14 @@
+package ru.amrxt.androidactivitypresentshippet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Secondary1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondary_1);
+    }
+}
