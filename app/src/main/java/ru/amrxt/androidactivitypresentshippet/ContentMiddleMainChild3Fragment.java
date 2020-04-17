f7f6a676cc547931f54f50e3f8b3fffdb424a5ac
amr <alexmakeresponsive@gmail.com>
2020-04-17 15:34:30 +0600

    add Widgets

--- app/src/main/java/ru/amrxt/androidactivitypresentshippet/ContentMiddleMainChild3Fragment.java
+++ app/src/main/java/ru/amrxt/androidactivitypresentshippet/ContentMiddleMainChild3Fragment.java
@@ -1,9 +1,12 @@
 package ru.amrxt.androidactivitypresentshippet;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import androidx.fragment.app.Fragment;
 
@@ -18,6 +21,15 @@ public class ContentMiddleMainChild3Fragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_content_middle_main_child_3, container, false);
 
+        VideoView videoView = view.findViewById(R.id.videoView);
+        String  videoPath = "android.resource://" + getActivity().getPackageName() + "/" + R.raw.video;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(getActivity());
+        videoView.setMediaController(mediaController);
+
+        mediaController.setAnchorView(videoView);
 
         return view;
     }
